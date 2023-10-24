0c44e8ab8ad9e03c007eff7f6ea03a40e90bc570
crrmacarse <crrmacarse@gmail.com>
2023-10-25 04:38:40 +0800

    feat: added navigation components

---
+++ src/components/navigation-header.tsx
@@ -0,0 +1,7 @@
+import { Text } from "react-native"
+
+const NavigationHeader = () => (
+    <Text style={{ color: '#fff', fontSize: 11 }}>@crrmacarse</Text>
+)
+
+export default NavigationHeader;
---
+++ src/components/navigation-title.tsx
@@ -0,0 +1,7 @@
+import { Text } from "react-native"
+
+const NavigationTitle = () => (
+    <Text style={{ color: '#fff', fontSize: 18, fontWeight: '800' }}>PASAJERO</Text>
+)
+
+export default NavigationTitle;
